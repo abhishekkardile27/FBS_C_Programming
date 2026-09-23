1e8ca3c7301ac3d0c088ecea4a627e0d9e6020e2
Abhishek Kardile <kardile2706@gmail.com>
2026-09-23 15:14:04 +0530

    Add files via upload
    2 nd assignment completed

---
+++ Assignment 2/StudentDiscount.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    float price, discount, finalPrice;
+    char student;
+
+    printf("Enter price: ");
+    scanf("%f", &price);
+
+    printf("Are you a student? (y/n): ");
+    scanf(" %c", &student);
+
+    if (student == 'y' || student == 'Y') {
+        if (price > 500) {
+            discount = price * 20 / 100;
+        }
+        else {
+            discount = price * 10 / 100;
+        }
+    }
+    else {
+        if (price > 600) {
+            discount = price * 15 / 100;
+        }
+        else {
+            discount = 0;
+        }
+    }
+
+    finalPrice = price - discount;
+
+    printf("Discount = %f\n", discount);
+    printf("Final Price = %f", finalPrice);
+
+    return 0;
+}
